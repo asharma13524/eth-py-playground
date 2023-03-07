b38eb1977f75c340e6dcc78e74b855d49cac68da
asharma13524 <asharma13524@gmail.com>
2023-03-06 19:43:39 -0500

    Add script that gets the largest txn by value in latest block

---
+++ play_script.py
@@ -0,0 +1,28 @@
+from dotenv import load_dotenv
+import os
+from web3 import Web3
+
+
+load_dotenv()
+
+api_key = os.getenv('API_KEY')
+
+# Connect to the Ethereum network
+w3 = Web3(Web3.HTTPProvider('https://mainnet.infura.io/v3/{}'.format(api_key)))
+
+# Get the latest block number
+latest_block = w3.eth.block_number
+
+# Get the latest block
+latest_block_obj = w3.eth.get_block(latest_block)
+
+# Find the transaction with the highest value in the latest block
+max_value_tx = None
+max_value = 0
+for tx_hash in latest_block_obj.transactions:
+    tx = w3.eth.get_transaction(tx_hash)
+    if tx.value > max_value:
+        max_value = tx.value
+        max_value_tx = tx
+        print("Transaction with highest value in block {}: {}".format(latest_block, max_value_tx))
+
